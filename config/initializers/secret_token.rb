99eeb8d16624f74ab74d98c95ff10a9ee93d3c96
zhangdavi <zhangdavidzh@gmail.com>
2013-10-30 20:47:53 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = '2d089a8b8b8305fe56a7dc6ff2346e43840a5f41b77ff919600ae5c67ebf450991850f0ce2e2f086a17c1aaaff28babf19a2e8e5ba39dc1b7308bdede20973b4'
---
+++ vendor/bundle/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20131030-3375-mh2u6m.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131030-3375-zvexky"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
